2f7c6ceb32668879cecadb003a9eefd726d01a33
Stoocea <1327956390@qq.com>
2024-03-08 10:34:35 +0800

    Site updated: 2024-03-08 10:34:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/post/FastJson反序列化.html","/post/RMI攻击总结.html","/post/JNDI注入.html","/post/blog迁移动态以及最近的事.html","/post/内存马-tomcat类全类型测试学习.html"];function toRandomPost(){
+var posts=["/post/Spring型内存马分析.html","/post/JavaAgent内存马.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
