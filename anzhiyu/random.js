045b66c5eead93fc3c57c2b3a59357a429227a7d
Stoocea <1327956390@qq.com>
2024-03-22 15:18:48 +0800

    Site updated: 2024-03-22 15:18:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/post/FastJsonRe0.html","/post/JNDIRe0.html","/post/Log4j漏洞分析.html","/post/JavaAgent内存马.html","/post/Jackson反序列化.html","/post/最近的感想.html","/post/RMIRe0.html","/post/Spring型内存马分析.html"];function toRandomPost(){
+var posts=["/post/C3P0链分析.html","/post/FastJsonRe0.html","/post/JNDIRe0.html","/post/JavaAgent内存马.html","/post/Log4j漏洞分析.html","/post/Jackson反序列化.html","/post/最近的感想.html","/post/Spring型内存马分析.html","/post/RMIRe0.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
